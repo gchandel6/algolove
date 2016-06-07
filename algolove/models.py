d4814025440e0c0cd9ff062f3318c35f778c99b3
gchandel6 <gchandel6@gmail.com>
2016-06-07 20:47:08 +0530

    Added code for recent activity

--- algolove/models.py
+++ algolove/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from pygments import lexers, formatters, highlight
-from tagging.fields import TagField
+#from tagging.fields import TagField
 from markdown import markdown
 from datetime import datetime,date
 
